636c20a3309930ba81df447eb5c72cb4827aa1ab
Bryan Robbins <bryantrobbins@gmail.com>
2016-11-29 12:13:04 -0600

    Add initial app from spring initializer

---
+++ src/main/java/com/btr3/NginxJavaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.btr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxJavaExampleApplication.class, args);
+	}
+}
